c9fc4352cefe67475aab5ddecb5934bac13e0afa
Frank Congson <fcongson@gmail.com>
2020-10-06 10:26:02 +1300

    Add styled system

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Button from './Button'
 import { ThemeProvider } from 'styled-components'
+import theme from '../../themes/default'
 import airbnb from '../../themes/airbnb'
 import figma from '../../themes/figma'
 import spotify from '../../themes/spotify'
@@ -15,57 +16,65 @@ export const Default = () => {
 }
 
 export const DefaultPrimary = () => {
-  return <Button type='primary'>Default primary</Button>
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant='primary'>Default primary</Button>
+    </ThemeProvider>
+  )
 }
 
 export const DefaultSecondary = () => {
-  return <Button type='secondary'>Default secondary</Button>
-}
-
-export const AirbnbPrimary = () => {
   return (
-    <ThemeProvider theme={airbnb}>
-      <Button type='primary'>Airbnb primary</Button>
+    <ThemeProvider theme={theme}>
+      <Button variant='secondary'>Default secondary</Button>
     </ThemeProvider>
   )
 }
 
-export const AirbnbSecondary = () => {
-  return (
-    <ThemeProvider theme={airbnb}>
-      <Button type='secondary'>Airbnb secondary</Button>
-    </ThemeProvider>
-  )
-}
+// export const AirbnbPrimary = () => {
+//   return (
+//     <ThemeProvider theme={airbnb}>
+//       <Button type='primary'>Airbnb primary</Button>
+//     </ThemeProvider>
+//   )
+// }
 
-export const FigmaPrimary = () => {
-  return (
-    <ThemeProvider theme={figma}>
-      <Button type='primary'>Figma primary</Button>
-    </ThemeProvider>
-  )
-}
+// export const AirbnbSecondary = () => {
+//   return (
+//     <ThemeProvider theme={airbnb}>
+//       <Button type='secondary'>Airbnb secondary</Button>
+//     </ThemeProvider>
+//   )
+// }
 
-export const FigmaSecondary = () => {
-  return (
-    <ThemeProvider theme={figma}>
-      <Button type='secondary'>Figma secondary</Button>
-    </ThemeProvider>
-  )
-}
+// export const FigmaPrimary = () => {
+//   return (
+//     <ThemeProvider theme={figma}>
+//       <Button type='primary'>Figma primary</Button>
+//     </ThemeProvider>
+//   )
+// }
 
-export const SpotifyPrimary = () => {
-  return (
-    <ThemeProvider theme={spotify}>
-      <Button type='primary'>Spotify primary</Button>
-    </ThemeProvider>
-  )
-}
+// export const FigmaSecondary = () => {
+//   return (
+//     <ThemeProvider theme={figma}>
+//       <Button type='secondary'>Figma secondary</Button>
+//     </ThemeProvider>
+//   )
+// }
 
-export const SpotifySecondary = () => {
-  return (
-    <ThemeProvider theme={spotify}>
-      <Button type='secondary'>Spotify secondary</Button>
-    </ThemeProvider>
-  )
-}
+// export const SpotifyPrimary = () => {
+//   return (
+//     <ThemeProvider theme={spotify}>
+//       <Button type='primary'>Spotify primary</Button>
+//     </ThemeProvider>
+//   )
+// }
+
+// export const SpotifySecondary = () => {
+//   return (
+//     <ThemeProvider theme={spotify}>
+//       <Button type='secondary'>Spotify secondary</Button>
+//     </ThemeProvider>
+//   )
+// }
---
+++ src/themes/default.ts
@@ -0,0 +1,12 @@
+export default {
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'black',
+    },
+    secondary: {
+      color: 'white',
+      bg: 'grey',
+    },
+  },
+}
